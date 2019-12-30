3c3e84c1caa5d6629e9425ae9aaa34fe088a63f1
Gregory Hazen <gmhazen@dmacc.edu>
2019-12-30 06:20:42 -0600

    Initial setup

---
+++ src/main/java/edu/dmacc/coma510/Main.java
@@ -0,0 +1,7 @@
+package edu.dmacc.coma510;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hangman!");
+    }
+}
---
+++ src/test/java/edu/dmacc/coma510/MainTest.java
@@ -0,0 +1,13 @@
+package edu.dmacc.coma510;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+    @Test
+    public void test() {
+        // Testing is ready for you if you'd like, but is NOT a requirement
+        assertEquals("Hangman", "Hangman");
+    }
+}
